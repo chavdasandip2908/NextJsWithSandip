7057a11b4a8965250ee8c42bbc13dae6512d8e9d
“Chavda <chavdasandip2908@gmail.com>
2024-06-25 17:34:13 +0530

    404 page

--- src/app/login/[...not-founds]/page.js
+++ src/app/login/[...not-founds]/page.js
@@ -1,9 +1,12 @@
 'use client'
 
+import Link from "next/link"
+
 export default function NotFound() {
     return (
         <div>
             <h2>This login page is not available</h2>
+            <Link href='/'>Go to home page</Link>
         </div>
     )
 }
