f2935b769e5afbe6324b0cf377bce09b6dd9a213
“Chavda <chavdasandip2908@gmail.com>
2024-07-02 15:26:41 +0530

    how to call  api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 
 'use client';
 
-
-import Link from 'next/link';
 import styles from './page.module.css'
+
 
 
 // export default function Home() {
@@ -72,27 +71,70 @@ import styles from './page.module.css'
 //   )
 // }
 
-import { useRouter } from 'next/navigation'
-export default function Home() {
-  const router = useRouter();
+// import Link from 'next/link';
+// import { useRouter } from 'next/navigation'
+// export default function Home() {
+//   const router = useRouter();
+//   return (
+//     <main className={styles.main}>
+//       <div>
+//         <h1>link and navigate and Nested Routing</h1>
+//         {/* using link */}
+//         <Link href="/login">Login</Link>
+//         <br />
+//         <Link href="/about">About</Link>
+//         <br />
+//         <Link href="/studentlist">Students</Link>
+//         <br />
+//         <br />
+//         <br />
+//         {/* using button */}
+//         <button onClick={()=>router.push("/login")}>Go to Login Page</button>
+//         <br />
+//         <button onClick={()=>router.push("/about")}>Go to About Page</button>
+//       </div>
+//     </main>
+//   )
+// }
+
+
+// fetch data api 
+
+
+import { useEffect, useState } from 'react';
+export default function page() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let data = await fetch('https://jsonplaceholder.typicode.com/todos');
+        data = await data.json();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
-    <main className={styles.main}>
-      <div>
-        <h1>link and navigate and Nested Routing</h1>
-        {/* using link */}
-        <Link href="/login">Login</Link>
-        <br />
-        <Link href="/about">About</Link>
-        <br />
-        <Link href="/studentlist">Students</Link>
-        <br />
-        <br />
-        <br />
-        {/* using button */}
-        <button onClick={()=>router.push("/login")}>Go to Login Page</button>
-        <br />
-        <button onClick={()=>router.push("/about")}>Go to About Page</button>
-      </div>
-    </main>
+    <div>
+      <h1>fetch data api</h1>
+      <h2>Product List</h2>
+      {
+        products.map((product, index) => (
+          <div key={index}>
+            <h4 >{product.id} : {product.title}</h4>
+            <p>{'=> '}{product.completed ? "Complet" : "Panding"}</p>
+            <br />
+            <br />
+          </div>
+        ))
+      }
+    </div>
   )
 }
